9d4a98ae001fd2e6e1a59e1ba76621d04c2d2b5a
FluidWizard-byte <blitzb4707912@gmail.com>
2024-12-11 16:57:09 +0545

    for deploy

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -41,6 +41,7 @@ let persons=[
 //   }
   
 app.use(express.json())
+app.use(express.static('dist'))
 //   app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
   
