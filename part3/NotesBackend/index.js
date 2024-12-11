f9099e903f27e20d4ebe13d47808db89eca19a12
FluidWizard-byte <blitzb4707912@gmail.com>
2024-12-11 14:18:30 +0545

    url change for production

--- part3/NotesBackend/index.js
+++ part3/NotesBackend/index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const morgan=require('morgan')
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 const PORT=3001
