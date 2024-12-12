e0b679e19c18c892d571208d2f93278f221ba1f7
FluidWizard-byte <blitzb4707912@gmail.com>
2024-12-12 19:43:34 +0545

    refactored and added changes
    refactored and added changes

--- part3/NotesBackend/index.js
+++ part3/NotesBackend/index.js
@@ -53,13 +53,9 @@ app.get('/api/notes',(request,response)=>{
 
 app.get('/api/notes/:id',(request,response)=>{
   const id=request.params.id
-  const note=notes.find(note=>note.id===id)
-  if(note){
+  Note.findById(id).then(note=>{
     response.json(note)
-  }
-  else{
-    response.status(404).end()
-  }
+  })
 })
 
 app.delete('/api/notes/:id',(request,response)=>{
@@ -68,31 +64,24 @@ app.delete('/api/notes/:id',(request,response)=>{
   response.status(204).end()
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
-}
-
 app.post('/api/notes', (request, response) => {
   const body = request.body
-
+  
   if (!body.content) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 
-  response.json(note)
 })
 
 app.use(unknownEndpoint)
