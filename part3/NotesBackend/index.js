9f84b940411828fad3043c3236d49f51e2965e97
FluidWizard-byte <blitzb4707912@gmail.com>
2024-12-14 13:37:28 +0545

    note validations
    note validations

--- part3/NotesBackend/index.js
+++ part3/NotesBackend/index.js
@@ -26,7 +26,9 @@ const errorHandler=(error,request,response,next)=>{
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'Invalid id' })
 } 
-
+else if(error.name==='ValidationError'){
+  return response.status(400).json({ error: error.message })
+}
 next(error)
 }
 
@@ -59,7 +61,7 @@ app.delete('/api/notes/:id',(request,response)=>{
   }).catch(error=>next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response,next) => {
   const body = request.body
   
   if (!body.content) {
@@ -75,19 +77,14 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote)
-  })
+  }).catch(error=>next(error))
 
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const{content,important}=request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, {content,important}, { new: true, runValidators:true,context:'query' })
     .then(updatedNote => {
       response.json(updatedNote)
     })
