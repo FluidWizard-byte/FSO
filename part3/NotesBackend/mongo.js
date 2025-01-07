930a2f2035479d62beac4f9464945e3c3fb30467
FluidWizard-byte <blitzb4707912@gmail.com>
2025-01-07 17:54:10 +0545

    try catch and more tests
    try catch and more tests

--- part3/NotesBackend/mongo.js
+++ part3/NotesBackend/mongo.js
@@ -7,7 +7,7 @@ if(process.argv.length<3){
 
 const password=process.argv[2]
 
-const url=`mongodb+srv://blitzb4707912:${password}@cluster0.qwotl.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url=`mongodb+srv://blitzb4707912:${password}@cluster0.qwotl.mongodb.net/noteAppTEST?retryWrites=true&w=majority&appName=Cluster0`
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
